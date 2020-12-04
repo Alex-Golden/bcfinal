dff9a1b875f68e5e1ef0d5024caaf2c11507caef
Alex Golden <goldenah@bc.edu>
2020-12-04 17:51:33 -0500

    Initial Commit

---
+++ Stadium Stacker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stadium Stacker
+//
+//  Created by Alex Golden on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stadium StackerTests/Stadium_StackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Stadium_StackerTests.swift
+//  Stadium StackerTests
+//
+//  Created by Alex Golden on 12/4/20.
+//
+
+import XCTest
+@testable import Stadium_Stacker
+
+class Stadium_StackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
